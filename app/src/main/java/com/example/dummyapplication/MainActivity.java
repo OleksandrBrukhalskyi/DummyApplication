56dd0f6b2fe71c61bbea2c811e1086a002be7b5f
OleksandrBrukhalskyi <bruhalskyi.o@gmail.com>
2023-03-06 19:26:54 +0200

    feat: implement mechanism of randomizing text and its color.

--- app/src/main/java/com/example/dummyapplication/MainActivity.java
+++ app/src/main/java/com/example/dummyapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dummyapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -10,16 +11,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
+public class MainActivity extends AppCompatActivity {
+    String[] tags;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView2);
+
+        tags = getResources().getStringArray(R.array.colors);
+        for(String tag : tags) {
+            String[] pair = tag.split(":");
+
+            String key = pair[0];
+            String value = pair[1];
+
+            // do whatever you want with key and value
+            textView.setText(key);
+            textView.setTextColor(Color.parseColor(value));
+        }
     }
 
+    private void updateTextView() {
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        Random random = new Random();
+        List<String> list = new ArrayList<>();
+        List<String> colors = new ArrayList<>();
+        for (String tag : tags) {
+            String[] pair = tag.split(":");
 
+            String key = pair[0];
+            String value = pair[1];
+            list.add(key);
+            colors.add(value);
+            // do whatever you want with key and value
+//            textView.setText(key);
+//            textView.setTextColor(Color.parseColor(value));
+
+
+        }
+        int maxIndex = list.size();
+        int colorIndex = colors.size();
+        int generatedIndex = random.nextInt(maxIndex);
+        int generatedIndexColor = random.nextInt(colorIndex);
+        textView.setText(list.get(generatedIndex));
+        textView.setTextColor(Color.parseColor(colors.get(generatedIndexColor)));
+
+    }
     public void clickYes(View view) {
+        updateTextView();
+
 //        Button button = findViewById(R.id.button);
 //        Drawable background = button.getBackground();
 //
@@ -35,6 +81,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickNo(View view) {
-
+        updateTextView();
     }
 }
